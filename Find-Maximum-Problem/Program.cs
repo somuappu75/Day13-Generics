439165ffdf504b7010cf280956f7d0754ea820af
Lenovo <somusp75@gmail.com>
2022-01-20 12:46:53 +0530

    Integer maximum method same tc

--- Find-Maximum-Problem/Program.cs
+++ Find-Maximum-Problem/Program.cs
@@ -2,11 +2,36 @@
 
 namespace Find_Maximum_Problem
 {
-    class Program
+ public class Program : IComparable
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //Console.WriteLine("Hello World!");
+        }
+       
+        /// <summary>
+        /// find maximun
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <param name="num3"></param>
+        /// <returns>maximum number</returns>
+        public int IntegerMaximum(int num1, int num2, int num3)
+        {
+            if (num1.CompareTo(num2) > 0)
+            {
+                return (num1.CompareTo(num3) > 0) ? num1 : num3;
+            }
+            else
+            {
+                return (num2.CompareTo(num3) > 0) ? num2 : num3;
+            }
+        }
+
+        int IComparable.CompareTo(object obj)
+        {
+            throw new NotImplementedException();
         }
     }
 }
